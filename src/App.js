deea157ace37b5b445a06a6774e4a69a37f117a7
pauline-wim <linette.wim@hotmail.com>
2022-01-26 14:57:42 +0100

    Add JSON & show users names, emails and websites

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from "react";
+import Users from "./users.json";
+// CSS
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+  constructor() {
+		super();
+
+		this.state = {Users};
+  };
+  
+  
+  render() {
+    return (
+      <div>
+        {this.state.Users.map((user) => {
+          return (
+            <div key={user.id}>
+              <p>{user.name}</p>
+              <p>{user.email}</p>
+              <p>{user.website}</p>
+            </div>
+          );
+        })};
+      </div>
+    ); 
+  };
+
+};
 
 export default App;
